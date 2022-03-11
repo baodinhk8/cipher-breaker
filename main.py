94a05756b413128f4c57e0ec506c33170340128d
Đinh Gia Bảo <dinhgiabao@inhs-MacBook-Pro.local>
2022-03-11 23:28:19 +0700

    j

--- main.py
+++ main.py
@@ -5,42 +5,46 @@ import time
 import requests
 import os
 
-def send_json_request(ws,request):
+
+def send_json_request(ws, request):
     ws.send(json.dumps(request))
 
+
 def receive_json_response(ws):
-    response=ws.recv()
+    response = ws.recv()
     if response:
         return json.loads(response)
 
-def heartbeat(interval,ws):
+
+def heartbeat(interval, ws):
     print("heartbeat begin")
     while True:
         time.sleep(interval)
-        heartbeatJSON={
-            "op":1,
-            "d":"null"
+        heartbeatJSON = {
+            "op": 1,
+            "d": "null"
         }
-        send_json_request(ws,heartbeatJSON)
+        send_json_request(ws, heartbeatJSON)
         print("Heartbeat sent")
 
-ws=websocket.WebSocket()
+
+ws = websocket.WebSocket()
 ws.connect("wss://gateway.discord.gg/?v=6&encording=json")
-event=receive_json_response(ws)
+event = receive_json_response(ws)
 
 heartbeat_interval = event['d']['heartbeat_interval']/1000
-threading._start_new_thread(heartbeat,(heartbeat_interval,ws))
+threading._start_new_thread(heartbeat, (heartbeat_interval, ws))
 
-token="OTUwMzExMTUxMTc3MTkxNDU2.YiXHDg.lVO7l0IeKOVw0rXrFYDOlJRR9vY"
+token = "OTUwMzExMTUxMTc3MTkxNDU2.YiXHDg.lVO7l0IeKOVw0rXrFYDOlJRR9vY"
 
-payload={
-    "op":2,
-    "d":{
-        "token":token,
-        "properties":{
-            "$os":"windows",
-            "$browser":"chrome",
-            "$device":"pc"
+payload = {
+    "op": 2,
+    "d": {
+        "token": token,
+        "properties": {
+            "$os": "windows",
+            "$browser": "chrome",
+            "$device": "pc"
         }
     }
 }
@@ -49,37 +53,52 @@ header = {
     'authorization': token,
 }
 
-send_json_request(ws,payload)
+send_json_request(ws, payload)
 
 while True:
-    event=receive_json_response(ws)
+    event = receive_json_response(ws)
 
     try:
         username = event['d']['author']['username']
         content = event['d']['content']
-        channel_id=event['d']['channel_id']
+        channel_id = event['d']['channel_id']
 
         f = open("log.csv", "a")
         f.write(channel_id+","+username+","+content+"\n")
         f.close()
-        
-        if username=="i have no brain" and content==".log":
+
+        if ".decode" in content:
+            content = content.lower()
+            a = content.split("$")
+            dc = ""
+            for char in a:
+                if char == " ":
+                    dc += " "
+                else:
+                    dc += str(int(char)-int('a'))+"-"
+            r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=header, data={
+                              "content": dc"})
+
+        if username == "i have no brain" and content == ".log":
             files = {
-                "file" : ("./log.csv", open("./log.csv", 'rb'))
+                "file": ("./log.csv", open("./log.csv", 'rb'))
             }
 
-            r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=header, files=files)
-        elif username=="i have no brain" and content==".status":
-            r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=header, data={"content":"Looking good, if I can survive until 1/4, this method will work fine, discord forget to fix bug, hopefully :D"})
-        elif username=="i have no brain" and content==".clear":
+            r = requests.post(
+                f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=header, files=files)
+        elif username == "i have no brain" and content == ".status":
+            r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=header, data={
+                              "content": "Looking good, if I can survive until 1/4, this method will work fine, discord forget to fix bug, hopefully :D"})
+        elif username == "i have no brain" and content == ".clear":
             os.remove("log.csv")
             f = open("log.csv", "a")
             f.write("channel_id,author,message_content\n")
             f.close()
-            r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=header, data={"content":"Done!"})
+            r = requests.post(
+                f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=header, data={"content": "Done!"})
 
-        op_code=event("op")
-        if op_code==11:
+        op_code = event("op")
+        if op_code == 11:
             print("heartbeat received")
-    except :
+    except:
         pass
